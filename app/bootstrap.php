2ebf0ccb81796e955cd9b64b7fbf4656de3faa9e
yourdesigncoza <laudes.michael@gmail.com>
2025-04-11 15:39:53 +0200

    Fixed view helpers implementation to properly expose functions in global namespace

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -77,21 +77,61 @@ function view($name, $data = []) {
  * Load view helpers
  */
 function load_view_helpers() {
+    // Include the view helpers file
     require_once WECOZA_APP_PATH . '/Helpers/ViewHelpers.php';
 
-    // Make helper functions available in the global namespace
-    foreach (get_defined_functions()['user'] as $function) {
-        $reflection = new \ReflectionFunction($function);
-        if ($reflection->getNamespaceName() === 'WeCoza\Helpers') {
-            $function_name = $reflection->getShortName();
-            if (!function_exists($function_name)) {
-                // Create a global function that calls the namespaced function
-                $namespace = $reflection->getNamespaceName();
-                $callback = "$namespace\\$function_name";
-                $GLOBALS[$function_name] = function() use ($callback) {
-                    return call_user_func_array($callback, func_get_args());
-                };
-            }
+    // Create function aliases in the global namespace
+    if (!function_exists('select_dropdown')) {
+        function select_dropdown($name, $options, $attributes = [], $selected = '', $empty_label = 'Select') {
+            return \WeCoza\Helpers\select_dropdown($name, $options, $attributes, $selected, $empty_label);
+        }
+    }
+
+    if (!function_exists('select_dropdown_with_optgroups')) {
+        function select_dropdown_with_optgroups($name, $optgroups, $attributes = [], $selected = '', $empty_label = 'Select') {
+            return \WeCoza\Helpers\select_dropdown_with_optgroups($name, $optgroups, $attributes, $selected, $empty_label);
+        }
+    }
+
+    if (!function_exists('form_input')) {
+        function form_input($type, $name, $label, $attributes = [], $value = '', $required = false, $invalid_feedback = 'Please fill out this field.', $valid_feedback = 'Looks good!') {
+            return \WeCoza\Helpers\form_input($type, $name, $label, $attributes, $value, $required, $invalid_feedback, $valid_feedback);
+        }
+    }
+
+    if (!function_exists('form_textarea')) {
+        function form_textarea($name, $label, $attributes = [], $value = '', $required = false, $invalid_feedback = 'Please fill out this field.', $valid_feedback = 'Looks good!') {
+            return \WeCoza\Helpers\form_textarea($name, $label, $attributes, $value, $required, $invalid_feedback, $valid_feedback);
+        }
+    }
+
+    if (!function_exists('form_group')) {
+        function form_group($type, $name, $label, $col_class = 'col-md-4', $attributes = [], $value = '', $required = false, $invalid_feedback = 'Please fill out this field.', $valid_feedback = 'Looks good!') {
+            return \WeCoza\Helpers\form_group($type, $name, $label, $col_class, $attributes, $value, $required, $invalid_feedback, $valid_feedback);
+        }
+    }
+
+    if (!function_exists('form_row')) {
+        function form_row($fields) {
+            return \WeCoza\Helpers\form_row($fields);
+        }
+    }
+
+    if (!function_exists('section_divider')) {
+        function section_divider($classes = '') {
+            return \WeCoza\Helpers\section_divider($classes);
+        }
+    }
+
+    if (!function_exists('section_header')) {
+        function section_header($title, $description = '', $title_tag = 'h5') {
+            return \WeCoza\Helpers\section_header($title, $description, $title_tag);
+        }
+    }
+
+    if (!function_exists('button')) {
+        function button($text, $type = 'button', $style = 'primary', $attributes = []) {
+            return \WeCoza\Helpers\button($text, $type, $style, $attributes);
         }
     }
 }
