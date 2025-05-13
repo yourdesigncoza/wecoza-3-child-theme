84205df689381f4f112fed2ebfa1bce248e62b4c
yourdesigncoza <laudes.michael@gmail.com>
2025-05-13 10:38:51 +0200

    Update styles and add new-look directory with prototype files

--- functions.php
+++ functions.php
@@ -13,6 +13,23 @@ define('WECOZA_PLUGIN_VERSION', $rand);
 define('WECOZA_CHILD_DIR', get_stylesheet_directory(__FILE__));
 define('WECOZA_CHILD_URL', get_stylesheet_directory_uri(__FILE__));
 
+
+
+/**
+ * Output Google Fonts preconnect and stylesheet tags.
+ */
+function ydcoza_google_fonts_links() {
+    ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
+    <?php
+}
+add_action( 'wp_head', 'ydcoza_google_fonts_links' );
+
+
+
+
 /**
  * Enqueue all necessary GLOBAL assets
  */
